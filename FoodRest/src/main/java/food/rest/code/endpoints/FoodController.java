c443879f39996674569af6452a49dc9a3c8dbdea
HazemSaeid <hazem2670@live.dk>
2019-12-08 19:51:58 +0100

    refactored endpoint

--- FoodRest/src/main/java/food/rest/code/endpoints/FoodController.java
+++ FoodRest/src/main/java/food/rest/code/endpoints/FoodController.java
@@ -4,21 +4,21 @@ import com.google.gson.Gson;
 import food.rest.code.Entities.Food;
 import food.rest.code.logic.JSONHandler;
 import org.json.simple.parser.ParseException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
-public class FoodEndpoints {
+@RequestMapping(path = "${v1API}/food")
+public class FoodController {
     ArrayList<Food> foods = new ArrayList<>();
     Gson gson;
     JSONHandler jsonHandler = new JSONHandler();
 
 
-    public FoodEndpoints() throws IOException, ParseException {
+    public FoodController() throws IOException, ParseException {
 
         this.foods = jsonHandler.jsonToArrayList(foods);
         this.gson = new Gson();
@@ -30,22 +30,22 @@ public class FoodEndpoints {
         return toString;
     }
 
-    @GetMapping("/food/category/{category}")
-    public String getCategory(@PathVariable String category){
-        ArrayList<Food> categorizedFoods = jsonHandler.getSpecificFoods(foods, category);
-        return gson.toJson(categorizedFoods);
-    }
-
-    @GetMapping("/food/names/{name}")
-    public String getFoodByName(@PathVariable String name){
-        ArrayList<Food> foodsByName = jsonHandler.getFoodsByName(foods, name);
-        return gson.toJson(foodsByName);
-    }
-
-    @GetMapping("/food/id/{id}")
+    @GetMapping("/{id}")
     public String getFoodById(@PathVariable long id){
         Food foodById = jsonHandler.getFoodById(foods, id);
         return gson.toJson(foodById);
     }
+    @GetMapping("/category")
+    public String getCategory(@RequestParam String name){
+        ArrayList<Food> categorizedFoods = jsonHandler.getSpecificFoods(foods, name);
+        return gson.toJson(categorizedFoods);
+    }
+
+    @GetMapping("/names")
+    public String getFoodByName(@RequestParam String name){
+        ArrayList<Food> foodsByName = jsonHandler.getFoodsByName(foods, name);
+        return gson.toJson(foodsByName);
+    }
+
 
 }
