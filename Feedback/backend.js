f4d0a706b76961b58528c68b6fe1d1862e171d5e
onkel dunkel <rasbalnor@gmail.com>
2019-12-11 12:19:42 +0100

    backend api working

--- Feedback/backend.js
+++ Feedback/backend.js
@@ -15,6 +15,13 @@ class ArgumentError extends Error {
         this.name = 'ArgumentError';
     }
 }
+class NotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.message = message;
+        this.name = 'NotFoundError';
+    }
+}
 
 const initialSetup = () => {
     db = new sqlite3.Database(dbName, (err)=> {
@@ -53,7 +60,6 @@ const convertToInt = (value, arguName) => {
 }
 
 const createFeedback = (rating, foodId, info = {}) => {
-
     rating = convertToInt(rating, 'rating');
     if (rating < 1 || rating > 5) {
         throw new ArgumentError(`rating must be in range 1-5. Actual: ${rating}`);
@@ -78,7 +84,71 @@ const createFeedback = (rating, foodId, info = {}) => {
         info.countryName, 
         info.upperAge, 
         info.lowerAge, 
-    ]);
+    ]).close();
+};
+
+const readFeedbackById = (id) => {
+    id = convertToInt(id, 'id');
+    const sql = `select * from feedback where id = ?;`;
+    const db = connect();
+
+    let feedback = {};
+    db.get(sql, [id], (err, row) => {
+        console.log(row);
+    }).close();
+    return feedback;
+};
+const readFeedbackByFoodId = (foodId) => {
+    foodId = convertToInt(foodId, 'foodId');
+    const sql = `select * from feedback where foodId = ?;`;
+    const db = connect();
+    let feedback = [];
+    db.all(sql, [foodId], (err, row) => {
+        console.log(row);
+    }).close();
+    return feedback;
+};
+const updateFeedback = (id, rating, foodId, info = {}) => {
+    id = convertToInt(id, 'id');
+    rating = convertToInt(rating, 'rating');
+    if (rating < 1 || rating > 5) {
+        throw new ArgumentError(`rating must be in range 1-5. Actual: ${rating}`);
+    }
+
+    foodId = convertToInt(foodId, 'foodId');
+
+    if (info.upperAge) info.upperAge = convertToInt(info.upperAge, 'upperAge');
+    if (info.lowerAge) info.lowerAge = convertToInt(info.lowerAge, 'lowerAge');
+
+    const sql = `
+    update feedback 
+        set foodId = ?, 
+        rating = ?, 
+        description = ?, 
+        gender = ?, 
+        countryName = ?, 
+        upperAge = ?, 
+        lowerAge = ? 
+    where id = ?;
+    `;
+    const db = connect();
+    db.run(sql, [
+        foodId, 
+        rating, 
+        info.description, 
+        info.gender, 
+        info.countryName, 
+        info.upperAge, 
+        info.lowerAge, 
+        id, 
+    ]).close();
+}
+
+const deleteFeedback = (id) => {
+    id = convertToInt(id, 'id');
+    const sql = `delete from feedback where id = ?;`;
+    const db = connect();
+    db.run(sql, [id]).close();
 };
 
 
@@ -86,5 +156,10 @@ const createFeedback = (rating, foodId, info = {}) => {
 
 exports.DBError = 'DBError';
 exports.ArgumentError = 'ArgumentError';
+exports.NotFoundError = 'NotFoundError';
 exports.initialSetup = initialSetup;
 exports.createFeedback = createFeedback;
+exports.readFeedbackById = readFeedbackById;
+exports.readFeedbackByFoodId = readFeedbackByFoodId;
+exports.updateFeedback = updateFeedback;
+exports.deleteFeedback = deleteFeedback;
--- Feedback/index.js
+++ Feedback/index.js
@@ -1,29 +1,4 @@
 const sqlite3 = require('sqlite3').verbose();
 const backend = require('./backend')
 	
-// let db = new sqlite3.Database('feedback.db');
-
 backend.initialSetup();
-
-backend.createFeedback('asdasd', 3, {
-    description : 'noice',
-    gender : 'nothing',
-    countryName : 'dk',
-    upperAge : 6,
-    lowerAge : '1asas'
-});
-	
-// let db = new sqlite3.Database('feedback.db');
-// console.log(db);
-
-
-// db.run('CREATE TABLE if not exists people(name text)');
-
-// db.run(`insert into people (name) values (?);`, ["bob"])
-
-// let sql = `select name from people where name = ?;`;
-// db.each(sql, ["bob"], (err, row) => {
-//     console.log(row)
-// });
-
-// db.close();
